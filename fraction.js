5f26b4ecb2fae04bbbe8576f046469fab2fb1a5b
sruthibasineni <119338968+sruthibasineni@users.noreply.github.com>
2023-04-20 11:56:45 +0530

    addtion fraction

--- fraction.js
+++ fraction.js
@@ -11,3 +11,17 @@ class Fraction{
 
 
 // adding fractions
+class AddTwoNumbers
+{
+  constructor(num1,num2){
+  this.num1=num1;
+  this.num2=num2;
+  }
+  
+  add(){
+    return this.num1 + this.num2;
+  }
+}
+let addtwonumbers= new AddTwoNumbers(2,3);
+let result=addtwonumbers.add();
+console.log(result);
